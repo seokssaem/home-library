457ca26c72179dcaf6289ba6f76b4b7c8758ba95
seokssaem <seokssaem@naver.com>
2026-09-04 12:46:04 +0900

    day83-260904-tests/test_main.py 수정

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,4 +17,4 @@ def test_하이픈이_있는_isbn13을_숫자로_정리한다():
 def test_체크섬이_틀린_isbn은_none을_반환한다():
     # 마지막 자리인 9만 0으로 바꿔서 체크섬이 깨지도록 해본다.
     # normalize_isbn은 숫자처럼 보이지만 가짜인 ISBN을 여기서 걸러내야 한다.
-    assert normalize_isbn('978-89-6626-318-0')
+    assert normalize_isbn('978-89-6626-318-0') is None
