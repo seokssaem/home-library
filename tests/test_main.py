630941b625c31a16475df89f0069e95d4be2f077
seokssaem <seokssaem@naver.com>
2026-09-04 15:16:53 +0900

    day83-260904-문제2풀이3

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,4 +23,4 @@ def test_ISBN10_유효한_경우_그대로_반환한다():
     assert normalize_isbn('0-306-40615-2') == '0306406152'
 
 def test_ISBN_체크자리가_X인_경우도_처리한다():
-    assert normalize_isbn('0-8044-2957-X') == '080442957X' # 소문자도 가능
+    assert normalize_isbn('0-8044-2957-x') == '080442957X' # 소문자도 가능
