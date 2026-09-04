3f7534122d166669feab16d5b36e113ab08c91ba
seokssaem <seokssaem@naver.com>
2026-09-04 15:07:47 +0900

    day83-260904-문제1풀이

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,3 +18,6 @@ def test_체크섬이_틀린_isbn은_none을_반환한다():
     # 마지막 자리인 9만 0으로 바꿔서 체크섬이 깨지도록 해본다.
     # normalize_isbn은 숫자처럼 보이지만 가짜인 ISBN을 여기서 걸러내야 한다.
     assert normalize_isbn('978-89-6626-318-0') is None
+
+def test_ISBN10_유효한_경우_그대로_반환한다():
+    assert normalize_isbn('0-306-40615-2') == '0306406152'
